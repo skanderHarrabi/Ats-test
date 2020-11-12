58c98bcc64df630f48f5be0d19be3568d796e4ec
skander <skanderharrabi75@gmail.com>
2020-11-12 17:56:59 +0100

    display products

--- client/src/pages/home-page/Home.js
+++ client/src/pages/home-page/Home.js
@@ -1,21 +1,70 @@
-import React from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Table, Tag, Space } from 'antd';
 import axiosInstance from "../../config/axios-instance";
 import './Home.scss';
 
 const Home = (props) => {
-    const handlClick = async () => {
-        const data = await axios(
-            {
-                method: 'get',
-                dataType: 'jsonp',
-                url: 'http://test.ats-digital.com:3000/products?size=100',
-            });
+    const [prods, setProds] = useState();
+    useEffect(() => {
+        const data = axiosInstance({
+            method: "get",
+            url: "/products"
+        }).then(res => {
+            console.log(res.data);
+            res.data.map(p => p.key = p._id);
+            setProds(res.data);
+        })
         console.log(data);
+    }, []);
+
+
+    const columns = [
+        {
+            title: 'productName',
+            dataIndex: 'productName',
+            key: 'productName',
+            render: productName => <a>{productName}</a>,
+        },
+        {
+            title: 'category',
+            dataIndex: 'category',
+            key: 'category',
+            render: category => <a>{category}</a>,
+        },
+        {
+            title: 'imageUrl',
+            dataIndex: 'imageUrl',
+            key: 'imageUrl',
+            render: imageUrl => <img src={imageUrl} alt='' className="image" />,
+
+        },
+        {
+            title: 'reviews',
+            key: 'reviews',
+            dataIndex: 'reviews',
+            render: imageUrl => <button onClick={(_id) => handlClickprod(_id)}>Show Reviews</button>,
+        }
+    ];
+
+
+    const handlClick = async () => {
+        const data = await axiosInstance({
+            method: "post",
+            url: "/products"
+        });
     }
+    const handlClickprod = async (record) => {
+        console.log(record);
+    }
+    // const data = prods.map(p => p.key = p._id);
     return (
         <div>
             <button onClick={handlClick}>Load Data</button>
+            {prods && <Table pagination={{ pageSize: 20 }} columns={columns} dataSource={prods} onRow={(record, rowIndex) => {
+                return {
+                    onClick: event => { console.log(event.target) }, // click row
+                };
+            }} />}
         </div>
     )
 }
--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,16 +1,13 @@
 const mongoose = require('mongoose');
-const { validationResult } = require('express-validator/check');
 const Products = mongoose.model('Products');
 const axios = require('axios');
 
 
 module.exports.products = async (req, res, next) => {
-    // console.log(req.body);
     const data = await axios.get('http://test.ats-digital.com:3000/products?size=100');
     data.data.products.map(p => {
         new Products(p).save();
     })
-    // const prods = new Products(req.body).save();
     console.log(data.data);
     res.json({ message: "success" });
 }
